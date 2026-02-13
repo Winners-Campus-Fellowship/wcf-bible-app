0d4cfbb3f480d55e412ed6ab80adc578590230e8
Jospin <jospin.muhanuzi@gmail.com>
2026-02-12 21:59:46 -0700

    Refactor component structure and enhance input handling for chapter fetching

--- code/src/lib/bible/suggestionUtils.ts
+++ code/src/lib/bible/suggestionUtils.ts
@@ -6,34 +6,34 @@ import bookMap from '$lib/shared/OSIS.json';
 
 /** Generates auto-suggestions for Bible verse references based on user input.*/
 export function generateAutoSuggestions(userInput: string): string[] {
-    const bcv = new bcv_parser(lang);
-    const {
-        passage: { books: parsed }
-    } = bcv.parse(userInput);
+	const bcv = new bcv_parser(lang);
+	const {
+		passage: { books: parsed }
+	} = bcv.parse(userInput);
 
-    if(!parsed[0]) return [];
+	if (!parsed[0]) return [];
 
-    let autoSuggestions: string[] = []; // clear the previous suggestions
+	let autoSuggestions: string[] = []; // clear the previous suggestions
 
-    if (parsed[0]) {
-        const parsedResult = parseQuery(userInput)!;
-        const passageReferences: string[] = parsed[0].parsed;
+	if (parsed[0]) {
+		const parsedResult = parseQuery(userInput)!;
+		const passageReferences: string[] = parsed[0].parsed;
 
-        // assume every suggested book has the requested chapter/verse
-        passageReferences.forEach((book) => {
-            if (parsedResult && parsedResult.verseProvided) {
-                autoSuggestions.push(
-                    `${bookMap[book as keyof typeof bookMap]} ${parsedResult.chapter}:${parsedResult.selectedVerse}`
-                );
-            } else if (parsedResult && !parsedResult.verseProvided) {
-                autoSuggestions.push(`${bookMap[book as keyof typeof bookMap]} ${parsedResult.chapter}`);
-            }
-        });
+		// assume every suggested book has the requested chapter/verse
+		passageReferences.forEach((book) => {
+			if (parsedResult && parsedResult.verseProvided) {
+				autoSuggestions.push(
+					`${bookMap[book as keyof typeof bookMap]} ${parsedResult.chapter}:${parsedResult.selectedVerse}`
+				);
+			} else if (parsedResult && !parsedResult.verseProvided) {
+				autoSuggestions.push(`${bookMap[book as keyof typeof bookMap]} ${parsedResult.chapter}`);
+			}
+		});
 
-        autoSuggestions = autoSuggestions
-            .filter((suggestion) => parseQuery(suggestion) !== null) // verify that the suggestions are valid
-            .sort();
-    }
+		autoSuggestions = autoSuggestions
+			.filter((suggestion) => parseQuery(suggestion) !== null) // verify that the suggestions are valid
+			.sort();
+	}
 
-    return autoSuggestions;
+	return autoSuggestions;
 }
--- code/src/routes/+page.svelte
+++ code/src/routes/+page.svelte
@@ -1,23 +1,19 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	
-	import Input from './Input.svelte';
+
+	import Input from './components/home/Input.svelte';
+	import Verse from './components/home/VerseNavigator.svelte';
 
 	import type { OSISReference } from '$lib/shared/format';
-	import { type BibleTranslation, type Verse } from '$lib/server/bible';
-	import { fetchChapter, getVerseReference } from '$lib/bible/chapterServices';
-	import YVTranslations from '$lib/shared/YVTranslations.json' with { type: 'json' };
-	import APIBibleTranslations from '$lib/shared/APIBibleTranslations.json' with { type: 'json' };
+	import { fetchChapter } from '$lib/bible/chapterServices';
+	import { type BibleTranslation, type Verse as VerseType } from '$lib/server/bible';
 
 	let verseLimit = 1;
 	let verseReference = '';
 	let osis: OSISReference;
-	let verseData: Verse[] = [];
+	let verseData: VerseType[] = [];
 	let selectedVerseIndex: number = 0;
 	let selectedTranslation: BibleTranslation = 'NIV';
-	const translations = [
-		...new Set([...Object.keys(YVTranslations), ...Object.keys(APIBibleTranslations)])
-	].sort();
 
 	async function fetchChapterData(query: string) {
 		const updatedData = await fetchChapter({
@@ -29,57 +25,29 @@
 			selectedTranslation
 		})!;
 
-		if(updatedData) {
+		if (updatedData) {
 			osis = updatedData.osis;
 			verseData = updatedData.verseData;
 			verseLimit = updatedData.verseLimit;
 			verseReference = updatedData.verseReference;
-			selectedVerseIndex = updatedData.selectedVerseIndex; 
-		}	
+			selectedVerseIndex = updatedData.selectedVerseIndex;
+		}
 	}
-	
+
 	onMount(async () => fetchChapterData('John 1:1')); // this will be the default verse
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-<Input 
-	fetchChapterData={fetchChapterData}
+<Input {fetchChapterData} />
+
+<Verse
+	{osis}
+	{verseData}
+	{verseLimit}
+	{verseReference}
+	{fetchChapterData}
+	{selectedVerseIndex}
+	{selectedTranslation}
 />
-
-<div>
-	<p>{verseReference}</p>
-	<p>{verseData[selectedVerseIndex]?.text}</p>
-</div>
-
-<button
-	id="button"
-	on:click={() => {
-		selectedVerseIndex -= 1;
-		verseReference = getVerseReference(verseData, osis, selectedVerseIndex);
-	}}
-	disabled={selectedVerseIndex == 0}>Previous</button
->
-<button
-	on:click={() => {
-		selectedVerseIndex += 1;
-		verseReference = getVerseReference(verseData, osis, selectedVerseIndex);
-	}}
-	disabled={selectedVerseIndex == verseLimit - 1}>Next</button
->
-
-<select
-	bind:value={selectedTranslation}
-	on:change={() => {
-		fetchChapterData(verseReference);
-
-		// verse reference is updated again to handle the case where the user changes to a translation that
-		// doesn't have the selected verse
-		verseReference = getVerseReference(verseData, osis, selectedVerseIndex);
-	}}
->
-	{#each translations as translation (translation)}
-		<option value={translation}>{translation}</option>
-	{/each}
-</select>
